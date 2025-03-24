11196ec6ec15eb8e513db8e25b8e677eed3cd24c
oscarfhdev <oscarfh.dev@gmail.com>
2025-03-24 09:19:39 +0100

    ejemplo_clase_guerreros_Z.sql --> add rows into table

---
+++ UT - 04/EJERCICIOS/ejemplo_clase_guerreros_Z.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS guerreros_z;
+CREATE DATABASE IF NOT EXISTS guerreros_z;
+USE guerreros_z;
+
+
+CREATE TABLE IF NOT EXISTS guerreros_z(
+    id_guerrero INT AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    raza VARCHAR(50) NOT NULL,
+    nivel_poder INT NOT NULL,
+    PRIMARY KEY (id_guerrero)
+);
+
+INSERT INTO guerreros_z (nombre, raza, nivel_poder) VALUES
+    ('Goku', 'Saiyan', 9500),
+    ('Vegeta', 'Saiyan', 9200),
+    ('Gohan', 'Saiyan-mestizo', 8700),
+    ('Piccolo', 'Namek', 7500),
+    ('Krilin', 'Humano', 4000);
+
+SELECT * FROM guerreros_z;
