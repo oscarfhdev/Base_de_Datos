713e73f3c38f201c73b66a8018d319591acd212a
oscarfhdev <oscarfh.dev@gmail.com>
2025-05-12 21:47:16 +0200

    UPDATE oscar_fernandez_hernandez_dql.sql --> Added correction for exercise 4

--- UT - 05/Examen_DDL_DML_DQL/oscar_fernandez_hernandez_dql.sql
+++ UT - 05/Examen_DDL_DML_DQL/oscar_fernandez_hernandez_dql.sql
@@ -15,7 +15,28 @@ ORDER BY total_creditos DESC;
 
 
 -- EJERCICIO 4
-SELECT g.nombre as nombre_grado, a.nombre as nombre_asignatura, MAX(a.creditos) as creditos FROM grado g
-LEFT JOIN asignatura a ON g.id = a.id_grado
-GROUP BY nombre_grado, nombre_asignatura 
-ORDER BY creditos DESC, nombre_asignatura;
+-- SELECT g.nombre as nombre_grado, a.nombre as nombre_asignatura, MAX(a.creditos) as creditos FROM grado g
+-- LEFT JOIN asignatura a ON g.id = a.id_grado
+-- GROUP BY nombre_grado, nombre_asignatura 
+-- ORDER BY creditos DESC, nombre_asignatura;
+
+
+
+-- CORRECIÓN CLASE
+SELECT g.nombre AS nombre_grado,
+    a.nombre as nombre_asignatura,
+    a.creditos AS creditos
+
+FROM grado g
+LEFT JOIN (
+    SELECT a1.*
+    FROM asignatura a1
+    WHERE a1.id = (
+        SELECT a2.id
+        FROM asignatura a2
+        WHERE a2.id_grado = a1.id_grado
+    ORDER BY a2.creditos DESC, a2.nombre ASC
+    LIMIT 1
+    )
+) a ON g.id = a.id_grado
+;
